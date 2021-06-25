18d3ac0e916b9627237b21525445113cfe9628e5
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-06-25 13:42:32 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF090C22),
+        scaffoldBackgroundColor: Color(0xFF090C21),
+
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
