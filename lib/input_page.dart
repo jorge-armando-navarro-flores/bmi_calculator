f316d461249786e06e614fc0583a615440a3c727
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-06-26 14:02:01 -0500

    ReusableCard_layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,8 +13,64 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(color:
+                  Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(color:
+                  Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(color:
+                  Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(color:
+                  Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(color:
+                  Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  ReusableCard({this.color = const Color(0xFF1D1E33)});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10)
       ),
     );
   }
